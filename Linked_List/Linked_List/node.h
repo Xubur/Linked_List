a7fe0a2aa8ca0cd6c37d3167690d74a5fb0c4e4d
link1371 <link1371@gmail.com>
2017-02-28 13:09:09 -0600

    Added Header for real this time

---
+++ Linked_List/Linked_List/node.h
@@ -0,0 +1,38 @@
+#ifndef _LINKEDLIST_H
+#define _LINKEDLIST_H
+#include <cstdlib>
+
+class node {
+public:
+	// TYPEDEF
+	typedef double value_type;
+	// CONSTRUCTOR
+	node(const value_type& init_data = value_type(), node* init_link = NULL)
+	{
+		data_field = init_data;
+		link_field = init_link;
+	}
+
+	// Member functions to set the data and link fields:
+	void set_data(const value_type& new_data)
+	{
+		data_field = new_data;
+	}
+	void set_link(node* new_link)
+	{
+		link_field = new_link;
+	}
+
+	// Constant member function to retrieve current data
+	value_type data() const
+	{
+		return data_field;
+	}
+
+private:
+	value_type data_field;
+	node *link_field;
+};
+#endif
+
+
